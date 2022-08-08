533be1f50a7d14c94fe24a001ee881c96d96663f
Moe <57568144+H4shByte@users.noreply.github.com>
2022-08-08 17:40:17 -0400

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,11 @@
+# prompts the user for input and then outputs that same input in lowercase.
+
+def main():
+    name = input("what is your input? ")
+    lowercase(name)
+
+def lowercase(x):
+    print(x.lower())
+    
+    
+main()
