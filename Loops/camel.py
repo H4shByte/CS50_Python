1b16e9cc18838249059c2351359f002cc829fd00
Moe <57568144+H4shByte@users.noreply.github.com>
2022-08-13 01:23:04 -0400

    Update camel.py

--- Loops/camel.py
+++ Loops/camel.py
@@ -5,9 +5,13 @@ def main():
 
 
 def camelcase(word):
+    # for each letter in word
      for i in word: 
+         # if letter is capitalized = True
          if i.isupper():
+             #print that letter in lower case and prevent print function from going to next line
              print("_" + i.lower(), end="")
+         #print letter
          else:
              print(i, end="")
 
