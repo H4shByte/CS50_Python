19387639c1d56da24bd83a47a8280d86ad850158
Moe <57568144+H4shByte@users.noreply.github.com>
2022-08-13 01:21:39 -0400

    Create camel.py

---
+++ Loops/camel.py
@@ -0,0 +1,14 @@
+def main():
+    string = input("camelcase: ")
+    camelcase(string)
+    print()
+
+
+ def camelcase(word):
+     for i in word: 
+         if i.isupper():
+             print("_" + i.lower(), end="")
+         else:
+             print(i, end="")
+
+main()
